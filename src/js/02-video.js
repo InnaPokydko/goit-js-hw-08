de8264f6258f53b89fe3a5a2d116ae4393fe8261
Inna Pokydko <inna.pokydko@gmail.com>
2023-02-15 20:13:23 +0200

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,13 @@
 import Player from '@vimeo/player';
-
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+player.on('play', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
