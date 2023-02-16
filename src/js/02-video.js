9afe8270ecede3de69b329c7130f1e76e23950da
Inna Pokydko <inna.pokydko@gmail.com>
2023-02-16 13:20:34 +0200

    update task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,9 +4,21 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-function onPlay({ timeupdate }) {
-  localStorage.setItem('videoplayer-current-time', timeupdate);
+function onPlay (timeupdate) {
+  localStorage.setItem('videoplayer-current-time',  timeupdate.seconds);
 }
 
 player.on('play', throttle(onPlay, 1000));
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function () {
+  // seconds = the actual time that the player seeked to
+}).catch(function (error) {
+  switch (error.name) {
+      case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+      default:
+          // some other error occurred
+          break;
+  }
+}); 
