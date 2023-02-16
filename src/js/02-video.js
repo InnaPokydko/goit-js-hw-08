58c04d78a9096f6d145dc4cd222194cc48b31079
Inna Pokydko <inna.pokydko@gmail.com>
2023-02-16 12:46:26 +0200

    fix task1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,9 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', throttle(onPlay, 1000));
-
-function onPlay({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
+function onPlay({ timeupdate }) {
+  localStorage.setItem('videoplayer-current-time', timeupdate);
 }
 
+player.on('play', throttle(onPlay, 1000));
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
