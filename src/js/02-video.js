71bfa8789d123b6b2db16163cf6598a5c261d32f
Inna Pokydko <inna.pokydko@gmail.com>
2023-02-18 15:42:41 +0200

    task2 uppdate

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,17 +2,41 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
+console.log('iframe');
+
 const player = new Player(iframe);
 
-player.on('play', function() {
-console.log('played video')
-});
+const currentTime = function (data) {
+    localStorage.setItem(
+      "videoplayer-current-time", JSON.stringify(data.seconds)
+    );
+    console.log(data);
+  };
 
-player.on('play', throttle(onPlay, 1000));
+const onPlay = player.on("timeupdate", throttle(currentTime, 1000));
 
-player.getVideoEl().then(function(data){
-  console.log('data:', data);
-});
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+
+// player.on('play', throttle(onPlay, 1000));
+
+// player.getVideoEl().then(function(data){
+//   console.log('data:', data);
+// });
 
 // (2 var)
 //  const currentTime = function (data) {
